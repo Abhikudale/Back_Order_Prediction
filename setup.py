83a0755fcb4499e8fe042e9763b9c626f362a165
Abhi <cv004700@gmail.com>
2022-06-22 22:04:28 +0530

    Updating requirements.txt

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from typing import List
 
 #Declaring variable for setup function
 PROJECT_NAME="housing-predictor",
-VERSION="0.0.1",
+VERSION="0.0.2",
 AUTHOR="Abhinandan Kudale",
 DESCRIPTION="This is a first FSDS Nov batch Machine Learning Project"
-PACKAGES=['housing']
 REQUIREMENT_FILE_NAME="requirements.txt"
 
 def get_requirements_list()->List[str]:
@@ -17,16 +16,14 @@ def get_requirements_list()->List[str]:
     of libraries mentioned in requirements.txt file
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines().remove("-e .")
 
 setup(
     name=PROJECT_NAME,
     version=VERSION,
     author=AUTHOR,
     description=DESCRIPTION,
-    packages=PACKAGES,
+    packages=find_packages(),
     install_requires=get_requirements_list()
 )
 
-if __name__=="__main__":
-    print(get_requirements_list)
