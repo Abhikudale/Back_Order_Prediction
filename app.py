f1d72877c2d84aad4e4992a6975941feabebd143
Abhi <cv004700@gmail.com>
2022-06-23 14:09:09 +0530

    Adding new folders and files

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from housing.logger import logging 
 
 app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
+    logging.info("We are testing logging module")
     return "Starting Machine Learning Project"
 
 if __name__=="__main__":
---
+++ housing/logger/__init__.py
@@ -0,0 +1,16 @@
+import logging
+from datetime import datetime
+import os
+
+LOG_DIR="housing_logs"
+
+CURRENT_TIMESTAMP= f"{datetime.now().strftime('%Y-%M-%d-%H-%M-%S')}"
+
+LOG_FILE_NAME=f"log_{CURRENT_TIMESTAMP}.log"
+
+os.makedirs(LOG_DIR,exist_ok=True)
+LOG_FILE_PATH = os.path.join(LOG_DIR,LOG_FILE_NAME)
+logging.basicConfig(filename=LOG_FILE_PATH,
+filemode="w",
+format='[%(sctime)s]%(name)s-%(levelname)s-%(message)s',
+level=logging.INFO)
