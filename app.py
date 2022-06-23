4617a287a8b4e2a025eb52f593f3ee176fff9776
Abhi <cv004700@gmail.com>
2022-06-23 15:16:24 +0530

    Folder structure completed

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
+import sys
 from flask import Flask
+import housing
+from housing.exception import HousingException
 from housing.logger import logging 
 
 app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    logging.info("We are testing logging module")
-    return "Starting Machine Learning Project"
+    try:
+        raise Exception("We are testing customer exception")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("We are testing Logging module")
+    return "CI CD Pipeline has been established."
 
 if __name__=="__main__":
     app.run(debug=True)
--- housing/exception/__init__.py
+++ housing/exception/__init__.py
@@ -5,7 +5,7 @@ class HousingException(Exception):
     
     def __init__(self, error_message:Exception,error_detail:sys):
        super.__init__(error_message)
-       self.error_message=error_message 
+       self.error_message=HousingException.get_detailed_error_message(error_message,error_detail) 
         
     
     @staticmethod
@@ -18,3 +18,6 @@ class HousingException(Exception):
     
     def __str__(self):
         return self.error_message
+
+    def __repr__(self) -> str:  
+        return HousingException.__name__.str()
