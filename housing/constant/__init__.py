26ffd0be2c706347628dc3a277aa293919c91ab0
Abhi <cv004700@gmail.com>
2022-07-06 17:37:16 +0530

    add new files

--- housing/constant/__init__.py
+++ housing/constant/__init__.py
@@ -32,3 +32,12 @@ DATA_VALIDATION_ARTIFACT_DIR_NAME="data_validation"
 DATA_VALIDATION_REPORT_FILE_NAME_KEY="report_file_name"
 DATA_VALIDATION_REPORT_PAGE_FILE_NAME_KEY="report_page_file_name"
 
+#Data Transformation variables
+DATA_TRANSFORMATION_ARTIFACT_DIR = "data_transformation"
+DATA_TRANSFORMATION_CONFIG_KEY = "data_transformation_config"
+DATA_TRANSFORMATION_ADD_BEDROOM_PER_ROOM_KEY = "add_bedroom_per_room"
+DATA_TRANSFORMATION_DIR_NAME_KEY = "transformed_data"
+DATA_TRANSFORMATION_TRAIN_NAME_DIR_KEY = "transformed_train_dir"
+DATA_TRANSFORMATION_TEST_NAME_DIR_KEY = "transformed_test_dir"
+DATA_TRANSFORMATION_PREPROCESSING_DIR_KEY = "preprocessing_dir"
+DATA_TRANSFORMATION_PREPROCESSED_OBJECT_FILE_NAME_KEY = "preprocessed_object_file_name"
