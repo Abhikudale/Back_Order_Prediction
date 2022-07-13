1a432eda15dcec3777e973acdf27a6f8eaa5e0b3
Abhi <cv004700@gmail.com>
2022-07-13 21:10:08 +0530

    Adding and updating app.py file

--- housing/constant/__init__.py
+++ housing/constant/__init__.py
@@ -1,12 +1,16 @@
 from datetime import datetime
 import os
 
-ROOT_DIR=os.getcwd()  #To get current working directory
-CONFIG_DIR="config"
-CONFIG_FILE_NAME="config.yaml"
-CONFIG_FILE_PATH=os.path.join(ROOT_DIR,CONFIG_DIR,CONFIG_FILE_NAME)
+def get_current_time_stamp():
+    return f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
 
-CURRENT_TIME_STAMP=f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
+    
+ROOT_DIR = os.getcwd()  #to get current working directory
+CONFIG_DIR = "config"
+CONFIG_FILE_NAME = "config.yaml"
+CONFIG_FILE_PATH = os.path.join(ROOT_DIR,CONFIG_DIR,CONFIG_FILE_NAME)
+
+CURRENT_TIME_STAMP = get_current_time_stamp()
 
 ##Training pipeline related variables:-
 TRAINING_PIPELINE_CONFIG_KEY="training_pipeline_config"
